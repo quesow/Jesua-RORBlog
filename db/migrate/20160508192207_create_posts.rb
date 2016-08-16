027871632ebe6270fde0663038e41415aa9238cb
JESUA BETANCOR ALEMAN <jbetancor.0011@gmail.com>
2016-08-16 23:17:04 +0100

    Post-user relationship and index fix

--- db/migrate/20160508192207_create_posts.rb
+++ db/migrate/20160508192207_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.belongs_to :user, index: true
       t.string :name
       t.string :title
       t.text :content
