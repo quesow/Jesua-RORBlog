2b2cb1241d79b5c5b88293912ac4be746877389f
JESUA BETANCOR ALEMAN <jbetancor.0011@gmail.com>
2016-08-17 22:19:33 +0100

    Add column name to user devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,70 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-#  before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  def new
-   super
-  end
-
-  # POST /resource
-  def create
-    @user = User.new(configure_sign_up_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to posts_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: posts_path }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  before_action :configure_sign_up_params, only: [:create]
 
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
