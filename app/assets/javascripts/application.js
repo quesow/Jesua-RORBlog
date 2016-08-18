54db428cbb6c5440c46af1f876bdd9286e01dfef
JESUA BETANCOR ALEMAN <jbetancor.0011@gmail.com>
2016-08-18 13:11:48 +0100

    Add datatables bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
